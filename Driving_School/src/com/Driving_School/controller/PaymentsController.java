dc6aca51775da85b24ff84880d1c9a072cf53540
sadeel16 <30291492+sadeel16@users.noreply.github.com>
2022-01-11 00:59:12 +0200

    payment

--- Driving_School/src/com/Driving_School/controller/PaymentsController.java
+++ Driving_School/src/com/Driving_School/controller/PaymentsController.java
@@ -1,13 +1,10 @@
 package com.Driving_School.controller;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
-import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -23,96 +20,75 @@ public class PaymentsController {
 
     @FXML
     private Label search_lb;
-
+    
+   
     @FXML
     private TextField search_name;
+    
+    @FXML
+    private TextField search_lastname;
+
 
     @FXML
     void search_Action(ActionEvent event) throws Exception {
     	Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drivingschool1","root","root");
     	Statement stmt = con.createStatement();
-    	String id = search_name.getText();
-    	String sql = "SELECT COUNT(*) lesson_id FROM vehicle_student where student_id ="+id ;
-    	String sql1 = "SELECT lesson_price FROM license_type where type_id = (select type_id from student_licensetype where student_id ="+id+");" ;
-    	String sql2 = "select amoumt from payments where student_id ="+id ;
+    	//String id = search_name.getText();
+    	String first_name = search_name.getText();
+    	String family_name = search_lastname.getText();
+    	
+    	String sql0 = "SELECT student_id FROM student where first_name = '%"+first_name+"%' and family_name = '%"+family_name+"%' " ;
+    	
+    	ResultSet rs0 = stmt.executeQuery(sql0);
+    	int id = 0;
+    	while (rs0.next()) {
+			id = rs0.getInt("student_id");
 
-    	ResultSet rs = stmt.executeQuery(sql);
+	      }
+    	rs0.close();
+
+    	
+    	String sql1 = "SELECT COUNT(*) lesson_id FROM vehicle_student where student_id ="+id ;
+    	String sql2 = "SELECT lesson_price FROM license_type where type_id = (select type_id from student_licensetype where student_id ="+id+");" ;
+    	String sql3 = "select amoumt from payments where student_id ="+id ;
+
+    	ResultSet rs1 = stmt.executeQuery(sql1);
     	int lesson_num = 0;
-    	while (rs.next()) {
-			lesson_num = rs.getInt("lesson_id");
+    	while (rs1.next()) {
+			lesson_num = rs1.getInt("lesson_id");
 	
 	      }
     	
-    	rs.close();
+    	rs1.close();
     	
-		ResultSet rs1 = stmt.executeQuery(sql1);
-		int lesson_price = 0;
-		while (rs1.next()) {
-			lesson_price = rs1.getInt("lesson_price");
-	
-	      }
-		
-		rs1.close();
 		ResultSet rs2 = stmt.executeQuery(sql2);
-
-		
-		
-		
-		int payed_amount = 0;
-
-		
-		
+		int lesson_price = 0;
 		while (rs2.next()) {
-			payed_amount = rs2.getInt("lesson_id");
+			lesson_price = rs2.getInt("lesson_price");
 	
 	      }
 		
 		rs2.close();
 		
-
-	
+		int payed_amount = 0;
 		
-		search_lb.setText(String.valueOf(payed_amount-(lesson_num*lesson_price)));
+		ResultSet rs3 = stmt.executeQuery(sql3);
+		while (rs3.next()) {
+			payed_amount = rs3.getInt("lesson_id");
+	
+	      }
+		
+		rs3.close();
+		
+		
+    	search_lb.setText(String.valueOf(payed_amount-(lesson_num*lesson_price)));
+
+
+		
     }
 	
-	
-//	@SuppressWarnings("unused")
-//	private static void calculations() throws SQLException {
-//	
-//		String name;
-//		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/drivingschool1","root","root");
-//		Statement stmt = con.createStatement();
-//		String sql = "SELECT COUNT(*) As s_num FROM student" ;
-//		ResultSet rs = stmt.executeQuery(sql);
-//		
-//		String sql1 = "SELECT student_id FROM student where first_name and family_name;" ;
-//		ResultSet rs1 = stmt.executeQuery(sql);
-//		
-//		while (rs.next()) {
-//	        int supplierID = rs.getInt("s_num");
-//	       
-//	        System.out.println(supplierID);
-//	      }
-//	
-//	}
-//	
-//	
 
 }
 
 	
-/*	private void checkLogin() throws IOException, SQLException, ClassNotFoundException {
-		Main m = new Main();
-		String userName = username.getText().toString();
-		String pass = password.getText().toString();
-		String sql = "Select * from employee Where emp_id='" + userName + "' and emp_id='" + pass + "'";
-		Connection conn = com.Driving_School.model.MySQLConnect.getConn();
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery(sql);
-		if (rs.next()) {
-			wrongLogIn.setText("Success!");
-			//change scene to the main page
-			m.changeScene("/com/Driving_School/view/AddNewEmployee.fxml");
-		}
 
-}*/
